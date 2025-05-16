7866836b7048d15e2d5f5960b977b3c37e891912
ybkk22 <138033309+ybkk22@users.noreply.github.com>
2025-05-15 23:47:00 -0300

    Update super_trunfo.c

--- super_trunfo.c
+++ super_trunfo.c
@@ -43,21 +43,31 @@ int main() {
     printf("Número de Pontos Turísticos: ");
     scanf("%d", &pontosTuristicosriodejaneiro);
 
-    // Exibindo informações da carta de São Paulo
+    // Cálculos
+    float densidadepopulacionalSaoPaulo = Populacaosaopaulo / areaSaoPaulo;
+    float densidadepopulacionalRioDeJaneiro = Populacaoriodojaneiro / areaRioDeJaneiro;
+
+    // PIB per capita corrigido (convertendo de bilhões para reais)
+    float pibpercapitaSaoPaulo = (pibSaoPaulo * 1000000000) / Populacaosaopaulo;
+    float pibpercapitaRioDeJaneiro = (pibRioDeJaneiro * 1000000000) / Populacaoriodojaneiro;
+    
+    // Exibição das cartas
     printf("\n--- INFORMAÇÕES DAS CARTAS ---\n");
-    printf("Carta %s - %s\n", A01, SaoPaulo);
+    printf("Carta 1: %s - %s\n", A01, SaoPaulo);
     printf("População: %d\n", Populacaosaopaulo);
     printf("Área: %.2f km²\n", areaSaoPaulo);
     printf("PIB: R$%.2f bilhões\n", pibSaoPaulo);
-    printf("Pontos turísticos: %d\n\n", pontosTuristicossaopaulo);
-
-    
-    // Exibindo informações da carta do Rio de Janeiro   
-    printf("Carta %s - %s\n", B02, RiodeJaneiro);
+    printf("Pontos turísticos: %d\n", pontosTuristicossaopaulo);
+    printf("Densidade populacional: %.2f hab/km²\n", densidadepopulacionalSaoPaulo);
+    printf("PIB per capita: R$%.2f reais\n\n", pibpercapitaSaoPaulo);
+    printf("Carta 2: %s - %s\n", B02, RiodeJaneiro);
     printf("População: %d\n", Populacaoriodojaneiro);
     printf("Área: %.2f km²\n", areaRioDeJaneiro);
     printf("PIB: R$%.2f bilhões\n", pibRioDeJaneiro);
     printf("Pontos turísticos: %d\n", pontosTuristicosriodejaneiro);
+    printf("Densidade populacional: %.2f hab/km²\n", densidadepopulacionalRioDeJaneiro);
+    printf("PIB per capita: R$%.2f reais\n\n", pibpercapitaRioDeJaneiro);
+    printf("\n--- FIM DAS INFORMAÇÕES ---\n");
     
 
     return 0;
